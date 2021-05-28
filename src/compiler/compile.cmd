a3236b49979ab67b2f7faefc22a20ba4d9551df9
firebolt360 <aadit.ambadkar@gmail.com>
2021-05-27 17:12:31 -0700

    Sample TEN file, cmd file, and ten.py run

---
+++ src/compiler/compile.cmd
@@ -0,0 +1,1 @@
+python "%~dp0ten.py" %1
---
+++ src/compiler/ten.py
@@ -0,0 +1,19 @@
+import sys
+import re
+from collections import deque
+
+filename = sys.argv[1]
+
+file = open(filename, "r")
+
+contents = file.read()
+
+q = deque()
+
+i = 0
+while (i<len(contents)):
+
+    i+=1
+
+print (contents)
+
