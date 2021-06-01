33acd4f679ae832e767a342b72e1e45d030284ba
firebolt360 <aadit.ambadkar@gmail.com>
2021-06-01 14:21:43 -0700

    Examples (and Loops?)

--- src/compiler/ten.py
+++ src/compiler/ten.py
@@ -33,10 +33,196 @@ types = {'i': int, 's': str, 'c': char}
 
 pointer = -1
 
+def boolexpr(s):
+    if (len(s)==0):
+        return True
+    qc = 0
+    i = 0
+    while (i < len(s)):
+        if (s[i]=="\""):
+            j = i
+            while (s[j]!="\'"):
+                j+=1
+            q.appendleft(s[i+1:j])
+            qc+=1
+            i=j
+        elif (s[i]=="^"):
+            if (q[0] in vars): 
+                q.appendleft(vars[q.popleft()])
+        elif (s[i]=="~"):
+            val = q.popleft()
+            if (val < 0):
+                q.appendleft(-1)
+            elif (val > 0):
+                q.appendleft(1)
+            else:
+                q.appendleft(0)
+        elif (s[i]=="]"):
+            q.appendleft(None)
+            qc+=1
+        elif (s[i]=="["):
+            q.appendleft(q.popleft()[int(q.popleft())])
+            qc-=1
+        elif (s[i]=="&"):
+            q.appendleft(q[0])
+            qc+=1
+        elif (s[i]=='+'):
+            q.appendleft(int(q.popleft())+int(q.popleft()))
+            qc-=1
+        elif (s[i]=='-'):
+            q.appendleft(int(q.popleft())-int(q.popleft()))
+            qc-=1
+        elif (s[i]=='/'):
+            q.appendleft(int(q.popleft())//int(q.popleft()))
+            qc-=1
+        elif (s[i]=='*'):
+            q.appendleft(int(q.popleft())*int(q.popleft()))
+            qc-=1
+        elif (s[i]=='('):
+            q.appendleft(q.popleft() and q.popleft())
+            qc-=1
+        elif (s[i]==')'):
+            q.appendleft(q.popleft() or q.popleft())
+            qc-=1
+        elif (s[i]=='.'):
+            q.appendleft(not q.popleft())
+        elif (s[i]=='='):
+            q.appendleft(q.popleft()==q.popleft())
+            qc-=1
+        elif (s[i]=='I'):
+            q.appendleft(int(q.popleft()))
+        elif (s[i]=='S'):
+            q.appendleft(str(q.popleft()))
+        elif (s[i]=='C'):
+            q.appendleft(char(str(q.popleft())))
+        else:
+            try: q.appendleft(int(s[i]))
+            except: pass
+        i+=1
+    ret = q.popleft()
+    qc-=1
+    # ideally, the below should not excecute (but it might ¯\_(ツ)_/¯)
+    while (qc>0):
+        q.popleft()
+        qc-=1
+    if (ret==True or ret==False): return ret
+    else: return True
+def exec(s):
+    i = 0
+    while (i<len(s)):
+        # print(q)
+        # print(vars)
+        if (s[i]=="@"):
+            while (s[i]!="%"):
+                i+=1
+        elif (s[i]=="\""):
+            j = i
+            while (s[j]!="\'"):
+                j+=1
+            q.appendleft(s[i+1:j])
+            i=j
+        elif (contents[i]=="|"):
+            i+=1
+            val = q.popleft()
+            # bval = False
+            try:
+                val = types[s[i]](val)
+            except:
+                pass
+            i+=1
+            j = i
+            while (s[j]!=":"):
+                j+=1
+            vars[s[i+1:j]]=val
+            i=j
+        elif (s[i]=="^"):
+            if (q[0] in vars): q.appendleft(vars[q.popleft()])
+        elif (s[i]==":"):
+            if (q[0] in vars): 
+                var = q.popleft()
+                val = q.popleft()
+                # print ((var, val))
+                vars[var]=val
+        elif (s[i]=="!"):
+            q.popleft()
+        elif (s[i]=="?"):
+            q.clear()
+        elif (s[i]=="~"):
+            val = q.popleft()
+            if (val < 0):
+                q.appendleft(-1)
+            elif (val > 0):
+                q.appendleft(1)
+            else:
+                q.appendleft(0)
+        elif (s[i]=="]"):
+            q.appendleft(None)
+        elif (s[i]=="["):
+            q.appendleft(q.popleft()[int(q.popleft())])
+        elif (s[i]==";"):
+            print(q[0])
+        elif (s[i]=="&"):
+            q.appendleft(q[0])
+        elif (s[i]=='+'):
+            q.appendleft(int(q.popleft())+int(q.popleft()))
+        elif (s[i]=='-'):
+            q.appendleft(int(q.popleft())-int(q.popleft()))
+        elif (s[i]=='/'):
+            q.appendleft(int(q.popleft())//int(q.popleft()))
+        elif (s[i]=='*'):
+            q.appendleft(int(q.popleft())*int(q.popleft()))
+        elif (s[i]=='('):
+            q.appendleft(q.popleft() and q.popleft())
+        elif (s[i]==')'):
+            q.appendleft(q.popleft() or q.popleft())
+        elif (s[i]=='.'):
+            q.appendleft(not q.popleft())
+        elif (s[i]=='='):
+            q.appendleft(q.popleft()==q.popleft())
+        elif (s[i]=='I'):
+            q.appendleft(int(q.popleft()))
+        elif (s[i]=='S'):
+            q.appendleft(str(q.popleft()))
+        elif (s[i]=='C'):
+            q.appendleft(char(str(q.popleft())))
+        elif (s[i]=='{'):
+            start = ""
+            j = i
+            while (s[j]!=','):
+                j+=1
+            start = s[i+1:j]
+            exec(start)
+            cond = ""
+            i=j
+            j+=1
+            while (s[j]!=','):
+                j+=1
+            cond = s[i+1:j]
+            i=j
+            j+=1
+            itr = ""
+            while (s[j]!=','):
+                j+=1
+            itr = s[i+1:j]
+            body = ""
+            i=j
+            j+=1
+            while (s[j]!='}'):
+                j+=1
+            body = s[i+1:j]
+            i=j
+            while (boolexpr(cond)):
+                exec(body)
+                exec(itr)
+        else:
+            try: q.appendleft(int(s[i]))
+            except: pass
+        i+=1
+
 i = 0
 while (i<len(contents)):
-    print(q)
-    print(vars)
+    # print(q)
+    # print(vars)
     if (contents[i]=="@"):
         while (contents[i]!="%"):
             i+=1
@@ -63,9 +249,13 @@ while (i<len(contents)):
     elif (contents[i]=="^"):
         if (q[0] in vars): q.appendleft(vars[q.popleft()])
     elif (contents[i]==":"):
-        if (q[0] in vars): vars[q.popleft()]=q.popleft()
+        if (q[0] in vars): 
+            var = q.popleft()
+            val = q.popleft()
+            # print ((var, val))
+            vars[var]=val
     elif (contents[i]=="!"):
-        del q[0]
+        q.popleft()
     elif (contents[i]=="?"):
         q.clear()
     elif (contents[i]=="~"):
@@ -92,6 +282,60 @@ while (i<len(contents)):
         q.appendleft(int(q.popleft())//int(q.popleft()))
     elif (contents[i]=='*'):
         q.appendleft(int(q.popleft())*int(q.popleft()))
+    elif (contents[i]=='('):
+        q.appendleft(q.popleft() and q.popleft())
+    elif (contents[i]==')'):
+        q.appendleft(q.popleft() or q.popleft())
+    elif (contents[i]=='.'):
+        q.appendleft(not q.popleft())
+    elif (contents[i]=='='):
+        q.appendleft(q.popleft()==q.popleft())
+    elif (contents[i]=='I'):
+        q.appendleft(int(q.popleft()))
+    elif (contents[i]=='S'):
+        q.appendleft(str(q.popleft()))
+    elif (contents[i]=='C'):
+        q.appendleft(char(str(q.popleft())))
+    elif (contents[i]=='{'):
+        start = ""
+        j = i
+        while (contents[j]!=','):
+            j+=1
+        start = contents[i+1:j]
+        # print(start)
+        exec(start)
+        cond = ""
+        i=j
+        j+=1
+        while (contents[j]!=','):
+            j+=1
+        cond = contents[i+1:j]
+        # print(cond)
+        i=j
+        itr = ""
+        j+=1
+        while (contents[j]!=','):
+            j+=1
+        itr = contents[i+1:j]
+        # print(itr)
+        body = ""
+        i=j
+        j+=1
+        while (contents[j]!='}'):
+            j+=1
+        body = contents[i+1:j]
+        # print(body)
+        i=j
+        cont = boolexpr(cond)
+        # print(cont)
+        while (cont):
+            exec(body)
+            # print("Body")
+            exec(itr)
+            cont = boolexpr(cond)
+            # print(cont)
+            # print(q)
+            # print(vars)
     else:
         try: q.appendleft(int(contents[i]))
         except: pass
@@ -101,6 +345,6 @@ while (i<len(contents)):
 
 print ()
 print ()
-print(q)
-print(vars)
+# print(q)
+# print(vars)
 input("End of Program, Press Return to Exit (Consumes CPU if you Don't) ")
